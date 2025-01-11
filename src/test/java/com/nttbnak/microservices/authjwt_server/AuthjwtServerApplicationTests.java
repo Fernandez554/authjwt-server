f767f14e1f052dd12441efd76deccbc977a7b3d7
Jesus Alberto Fernandez Malpartida <jesus.fernandez.malpartida@gmail.com>
2025-01-11 11:11:36 -0500

    Initial commit: base project authentication server

---
+++ src/test/java/com/nttbnak/microservices/authjwt_server/AuthjwtServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttbnak.microservices.authjwt_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthjwtServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
